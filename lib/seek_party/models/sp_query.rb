6fb5d7727069b298be99575b248f06158a53c0a6
Charles Washington <charles_was_7@hotmail.com>
2019-12-25 09:35:52 -0300

    Code improvement suggestion

--- lib/seek_party/models/sp_query.rb
+++ lib/seek_party/models/sp_query.rb
@@ -15,18 +15,15 @@ class SPQuery
   end
 
   def build_final_query
-    final_query = ''
-    @queries.each do |_key, value|
-      next unless value
-
-      final_query << if value == @queries[@queries.keys.first]
-                       value
-                     else
-                       @params[:search].present? ? " OR #{value}" : " AND #{value}"
-                     end
-    end
-
-    final_query
+    @queries.map { |key, value|
+      if key == @queries.keys.first
+        value
+      else
+        or_and(value)
+      end }.sum
   end
 
+  def or_and(value)
+    @params[:search].present? ? " OR #{value}" : " AND #{value}"
+  end
 end
